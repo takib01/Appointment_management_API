d99d1421ca3c63b509f0b494f298a9337976a8f5
Takib Khan <c213101@ugrad.iiuc.ac.bd>
2025-08-19 23:04:39 +0600

    Refactor build script and update requirements to remove unused packages

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Clear pip cache to avoid conflicts
+pip cache purge
+
+# Install dependencies
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
